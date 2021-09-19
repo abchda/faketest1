c0bec427922829e3a056b8500a8d3cdc6a811fcf
Tun1ck <listov0@bk.ru>
2021-09-19 17:52:33 +0300

    мы создали сервер

---
+++ WebApplication1/Controllers/BudgetContext.cs
@@ -0,0 +1,20 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Budget> Budgets { get; set; }
+    }
+}
